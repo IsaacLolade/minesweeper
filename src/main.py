d456cca0799b241dc0962541142ae3d6851d06ec
CharlyMech <sanchezreciocarlos99@gmail.com>
2022-12-25 12:41:39 +0100

    SRC folder and final directory structure. Lots of icons added pendent to decide which one to use. Currently working on mouse click detection on test files

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from functools import partial
+from pynput import mouse
+import random
+
+
+if __name__ == "__main__":
+    game = [[0]*8]*8
+
+    for i in game:
+        print(i)
