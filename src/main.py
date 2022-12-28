bd5eb2d6f709e2a13f7a884c044238c3208a8b68
CharlyMech <sanchezreciocarlos99@gmail.com>
2022-12-28 11:07:55 +0100

    Updated timer -> No need to sleep() now, using time() instead, better feeling in the game

--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
 from tkinter import *
 from functools import partial
 from pynput import mouse
+from time import time
 import random
+
+
+def count_mines(game_dict: dict) -> int:
+    mines_count = 0
+    for rv in game_dict.values():
+        for cv in rv.values():
+            if cv[0] == 'm':
+                mines_count += 1
+
+    return mines_count
+
+
+def put_mines(game_dict: dict) -> None:
+    mines_count = count_mines(game_dict)
+    while mines_count < 10:
+        rand_x = random.randint(0, 7)
+        rand_y = random.randint(0, 7)
+
+        game_dict[rand_x][rand_y][0] = 'm'
+
+    # return game_dict
 
 
 def create_game_table(game_dict: dict) -> None:
@@ -13,6 +35,26 @@ def create_game_table(game_dict: dict) -> None:
             # textvariable=globals()[f"btn_{r_key-c_key}"] -> Button textvariable
             Button(window, image=c_val[1]).grid(
                 row=r_key+1, column=c_key)
+
+
+def timer():
+    global init_seconds, stop
+
+    # sleep(1)
+    seconds = round(time()) - init_seconds
+
+    if stop == 0:
+        lbl = Label(window, text=f"{seconds}", justify=CENTER,
+                    font=('KacstDigital', 20), bg='black', fg='red')
+        lbl.after(200, timer)
+        lbl.grid(row=0, column=5, columnspan=2, padx=10, pady=10)
+
+
+def restart():
+    global stop
+    stop = 1
+    seconds.set(0)
+    stop = 0
 
 
 if __name__ == "__main__":
@@ -28,13 +70,18 @@ if __name__ == "__main__":
     remaining_mines = StringVar(window)
     remaining_mines.set(10)
 
+    init_seconds = round(time())
+    stop = 0
+
+    timer()
+
     game = {}
     # * This is for my personal folder open on VSCode (on Programming subject directory)
     def_img = PhotoImage(
-        file="01 Python/Actividades/MinesWeeper/src/icons/png/24/tile.png")
+        file="01 Python/Actividades/MinesWeeper/src/icons/24/tile.png")
 
     restart_img = PhotoImage(
-        file="01 Python/Actividades/MinesWeeper/src/icons/png/24/smiley.png")
+        file="01 Python/Actividades/MinesWeeper/src/icons/24/smiley.png")
 
     # * This is the path for the standalone project
     # def_img = PhotoImage(
@@ -48,16 +95,14 @@ if __name__ == "__main__":
         for c in range(8):
             game[r][c] = ["0", def_img]
 
-    # Label(window, text="GAME TOP BAR").grid(
-    #     row=0, column=0, columnspan=8, pady=10)
-
     Label(window, textvariable=remaining_mines, justify=CENTER,
           font=('KacstDigital', 20), bg='black', fg='red').grid(row=0, column=1, columnspan=2, padx=10, pady=10)
 
-    Button(window, image=restart_img).grid(row=0, column=3, columnspan=2)
+    restart_btn = Button(window, image=restart_img, command=restart).grid(
+        row=0, column=3, columnspan=2)
 
-    Label(window, textvariable=remaining_mines, justify=CENTER,
-          font=('KacstDigital', 20), bg='black', fg='red').grid(row=0, column=5, columnspan=2, padx=10, pady=10)
+    # Label(window, textvariable=seconds, justify=CENTER,
+    #       font=('KacstDigital', 20), bg='black', fg='red').grid(row=0, column=5, columnspan=2, padx=10, pady=10)
 
     create_game_table(game)
 
