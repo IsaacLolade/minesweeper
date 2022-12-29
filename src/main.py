61c48df05286699d1099a451bda0c8cfeceae49c
CharlyMech <sanchezreciocarlos99@gmail.com>
2022-12-29 19:39:41 +0100

    Done with game table, nums are set and calculated. Little bug on clock. Still pendent to take care of buttons and final game engine

--- src/main.py
+++ src/main.py
@@ -24,7 +24,134 @@ def put_mines(game_dict: dict) -> None:
         game_dict[rand_x][rand_y][0] = 'm'
         mines_count += 1
 
-    # return game_dict
+
+def check_near_mines(game_dict: dict) -> None:
+    for r_key, r_val in game_dict.items():
+        for c_key, c_val in r_val.items():
+            if c_val[0] == 'm':
+                continue
+
+            # First set of cases: r = 0
+            if r_key == 0:
+                # c = 0
+                if c_key == 0:
+                    # - This way everyone is evaluated
+                    if r_val[c_key + 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key + 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key + 1][c_key + 1][0] == 'm':
+                        c_val[0] += 1
+                # c = 7
+                elif c_key == 7:
+                    if r_val[c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key + 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key + 1][c_key - 1][0] == 'm':
+                        c_val[0] += 1
+                # c = n
+                else:
+                    if r_val[c_key + 1][0] == 'm':
+                        c_val[0] += 1
+                    if r_val[c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key + 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key + 1][c_key + 1][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key + 1][c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+            # Second set of cases: r = 7
+            elif r_key == 7:
+                # c = 0
+                if c_key == 0:
+                    # - This way everyone is evaluated
+                    if r_val[c_key + 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key - 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key - 1][c_key + 1][0] == 'm':
+                        c_val[0] += 1
+                # c = 7
+                elif c_key == 7:
+                    if r_val[c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key - 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key - 1][c_key - 1][0] == 'm':
+                        c_val[0] += 1
+                # c = n
+                else:
+                    if r_val[c_key + 1][0] == 'm':
+                        c_val[0] += 1
+                    if r_val[c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key - 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key - 1][c_key + 1][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key - 1][c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+            # Last set of cases: r = n
+            else:
+                # c = 0
+                if c_key == 0:
+                    # - This way everyone is evaluated
+                    if r_val[c_key + 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key + 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key + 1][c_key + 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key - 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key - 1][c_key + 1][0] == 'm':
+                        c_val[0] += 1
+                # c = 7
+                elif c_key == 7:
+                    if r_val[c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key + 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key + 1][c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key - 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key - 1][c_key - 1][0] == 'm':
+                        c_val[0] += 1
+                # c = n
+                else:
+                    if r_val[c_key + 1][0] == 'm':
+                        c_val[0] += 1
+                    if r_val[c_key - 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key + 1][c_key - 1][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key + 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key + 1][c_key + 1][0] == 'm':
+                        c_val[0] += 1
+
+                    if game_dict[r_key - 1][c_key - 1][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key - 1][c_key][0] == 'm':
+                        c_val[0] += 1
+                    if game_dict[r_key - 1][c_key + 1][0] == 'm':
+                        c_val[0] += 1
 
 
 def create_game_table(game_dict: dict) -> None:
@@ -36,6 +163,9 @@ def create_game_table(game_dict: dict) -> None:
             # textvariable=globals()[f"btn_{r_key-c_key}"] -> Button textvariable
             Button(window, image=c_val[1], textvariable=globals()[f"btn_{r_key-c_key}"], command=partial(print_but_value, globals()[f"btn_{r_key-c_key}"].get())).grid(
                 row=r_key+1, column=c_key)
+            # * Test if all values are calculated correctly
+            # Button(window, text=f"{c_val}", textvariable=globals()[f"btn_{r_key-c_key}"], command=partial(print_but_value, globals()[f"btn_{r_key-c_key}"].get())).grid(
+            #     row=r_key+1, column=c_key)
 
 # Test function
 
@@ -44,10 +174,11 @@ def print_but_value(param):
     print(param)
 
 
-def timer():
+def timer() -> None:
+    # TODO - bug over 100 secs, restets to 100 no 0
+    # Also reset not working properly
     global init_seconds, stop
 
-    # sleep(1)
     seconds = round(time()) - init_seconds
 
     if stop == 0:
@@ -57,7 +188,7 @@ def timer():
         lbl.grid(row=0, column=5, columnspan=2, padx=10, pady=10)
 
 
-def restart():
+def restart() -> None:
     global stop, init_seconds
     stop = 1
     init_seconds = round(time())
@@ -100,7 +231,7 @@ if __name__ == "__main__":
     for r in range(8):
         game[r] = {}
         for c in range(8):
-            game[r][c] = ["0", def_img]
+            game[r][c] = [0, def_img]
 
     Label(window, textvariable=remaining_mines, justify=CENTER,
           font=('KacstDigital', 20), bg='black', fg='red').grid(row=0, column=1, columnspan=2, padx=10, pady=10)
@@ -108,10 +239,11 @@ if __name__ == "__main__":
     restart_btn = Button(window, image=restart_img, command=restart).grid(
         row=0, column=3, columnspan=2)
 
-    # Label(window, textvariable=seconds, justify=CENTER,
-    #       font=('KacstDigital', 20), bg='black', fg='red').grid(row=0, column=5, columnspan=2, padx=10, pady=10)
-
+    # Introduce mines into the game dict
     put_mines(game)
+    # Update values to counte mines around
+    check_near_mines(game)
+    # Create the game table UI
     create_game_table(game)
 
     window.mainloop()
